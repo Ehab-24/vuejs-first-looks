7b0c529c34f1b1bc1587750569e8f892ebdd6918
Ehab-24 <ehabs1775@gmail.com>
2023-02-21 16:46:21 +0500

    first commit by yours truly

---
+++ src/components/EventButton.vue
@@ -0,0 +1,22 @@
+<script>
+
+
+export default {
+    props: {
+        text: {
+            type: String,
+            default: "button",
+        },
+        event: {
+            type: String,
+        }
+    },
+};
+</script>
+
+
+<template>
+    <button @click="$emit(event)" class="mt-4 h-10 w-28 hover:w-32 text-white bg-orange-700 rounded hover:rounded-3xl transition-all">
+        {{ text }}
+    </button>
+</template>
