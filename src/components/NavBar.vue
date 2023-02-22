521e24ebd08c4caff9f7ff909d46de3ae7e213bb
Ehab-S24 <122555862+Ehab-S24@users.noreply.github.com>
2023-02-22 13:10:01 +0500

    Navbar Completed (#2)
    * navbar completed
    
    * Deleted Heading.vue and added a navbar to App.vue.
    
    - The Heading.vue file was deleted because it was not being used and did not need its separate component.
    - The navbar was added to App.vue as mentioned in the project work plan.
    
    ---------
    
    Co-authored-by: Ehab-24 <ehabs1775@gmail.com>

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<script>
+export default {};
+</script>
+
+
+
+<template>
+  <ul class="h-12 w-[560px] flex items-center justify-between text-black font-bold text-lg">
+    <li
+      class="hover:scale-x-[1.1] cursor-pointer transition-all"
+    >
+      Dashboard
+    </li>
+    <li
+      class="hover:scale-x-[1.1] cursor-pointer transition-all"
+    >
+      Products
+    </li>
+    <li
+      class="hover:scale-x-[1.1] cursor-pointer transition-all"
+    >
+      Debt
+    </li>
+    <li
+      class="hover:scale-x-[1.1] cursor-pointer transition-all"
+    >
+      Account
+    </li>
+  </ul>
+</template>
